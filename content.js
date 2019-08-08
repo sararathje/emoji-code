3ea1bf4e432d20671d75b3d787e80495bcae36e0
Sara Rathje <sararathje@gmail.com>
2019-08-07 18:10:22 -0600

    Partially working suggestion popup. Does not include any sort of filtering, but shows first 5 suggestions

--- content.js
+++ content.js
@@ -15,16 +15,39 @@
   const COLON_KEY = ':';
   const SHIFT_KEY = 'Shift';
   const BACKSPACE_KEY = 'Backspace';
+  const ARROW_UP_KEY = 'ArrowUp';
+  const ARROW_DOWN_KEY = 'ArrowDown';
+  const ENTER_KEY = 'Enter';
 
-  let prevMessageText;
-  let lastKeyPressed;
-  let shouldInsertLastTypedKey = false;
+  let messageInputElement;
+
+  const state = {
+    emojiSelectionOpen: false,
+    shouldInsertLastTypedKey: false,
+    prevMessageText: '',
+    lastKeyPressed: '',
+    selectedEmojiIndex: 0,
+    suggestedEmojiList: [],
+    messengerInputElement: ''
+  }
 
   document.onkeyup = handleKeyEvent;
   document.onkeydown = updatePrevStoredValues;
+  document.onmouseup = handleMouseUpEvent;
 
   function handleKeyEvent(event) {
     let messageText = document.activeElement.textContent;
+
+    const emojiSelectionWrapper = document.getElementById('emoji-selection-wrapper');
+
+    // Sara you need a better system... this really sucks.
+    // Removed for now but you should put back after 
+    if (emojiSelectionWrapper !== undefined && emojiSelectionWrapper !== null) {
+      if (emojiSelectionWrapper.className === 'active') {
+        checkForArrowKeys(event, emojiSelectionWrapper);
+        checkForEnterKey(event, emojiSelectionWrapper);
+      }
+    }
 
     // Sometimes a delete will cause the next key pressed to not be rendered to the
     // screen. On the keyup, check to see if the key pressed is correctly rendered. 
@@ -39,6 +62,104 @@
     }
   }
 
+  // Sara: Something in the arrow keys is broken. Try to fix.
+  function checkForArrowKeys(event, emojiSelectionWrapper) {
+    const emojiSelectionOptions = emojiSelectionWrapper.childNodes;
+    const currentSelectedEmoji = 
+          document.getElementsByClassName('emoji-suggestion-selected')[0];
+
+    if (event.key === ARROW_DOWN_KEY) {
+      if (state.selectedEmojiIndex !== emojiSelectionOptions.length - 1) {
+        state.selectedEmojiIndex++;
+        
+        currentSelectedEmoji.className = 'emoji-suggestion';
+
+        emojiSelectionOptions[state.selectedEmojiIndex].className = 'emoji-suggestion-selected';
+      }
+    } else if (event.key === ARROW_UP_KEY) {
+      if (state.selectedEmojiIndex !== 0) {
+        state.selectedEmojiIndex --;
+
+        currentSelectedEmoji.className = 'emoji-suggestion';
+
+        emojiSelectionOptions[state.selectedEmojiIndex].className = 'emoji-suggestion-selected';
+      }
+    }
+  }
+
+  function checkForEnterKey(event, emojiSelectionWrapper) {
+    if (event.key === ENTER_KEY) {
+      selectEmojiAndClose(emojiSelectionWrapper);
+    }
+  }
+
+  function handleMouseUpEvent(event) {
+    // Any time there's a mouseup event we want to close the emoji suggestions. If the target is
+    // the suggestion wrapper, then select the result. Otherwise, just close without doing
+    // anything.
+    // alert('should be handling mouse event???');
+    const emojiSelectionWrapper = document.getElementById('emoji-selection-wrapper');
+
+    if (emojiSelectionWrapper !== undefined && emojiSelectionWrapper !== null) {
+      if (emojiSelectionWrapper.className === 'active') {
+        if (event.target === emojiSelectionWrapper || emojiSelectionWrapper.contains(event.target)) {
+          selectEmojiAndClose(emojiSelectionWrapper, state.suggestedEmojiList[state.selectedEmojiIndex]);
+        } else {
+          closeEmojiSuggestions(emojiSelectionWrapper);
+        }
+      }
+    }
+  }
+
+  function selectEmojiAndClose(emojiSelectionWrapper) {
+    const selectedEmoji = document.getElementsByClassName('emoji-suggestion-selected')[0];
+    // alert(selectedEmoji);
+    const emoji = state.suggestedEmojiList[state.selectedEmojiIndex];
+
+    // Close the emojiSelection thing
+    closeEmojiSuggestions(emojiSelectionWrapper, selectedEmoji);
+
+    messengerInputElement.focus();
+    const currentText = document.activeElement.textContent;
+    const updatedText = `${currentText}${emoji}`;
+
+    document.execCommand('selectAll');
+    document.execCommand('insertText', true, updatedText);
+  }
+
+  function closeEmojiSuggestions(emojiSelectionWrapper, selectedEmoji) {
+    emojiSelectionWrapper.className = '';
+    selectedEmoji.className = 'emoji-suggestion';
+    state.selectedEmojiIndex = 0;
+  }
+
+  function showEmojiSuggestions() {
+    // alert('should be showing emojis!!!');
+    const emojiSelectionWrapper = document.getElementById('emoji-selection-wrapper');
+    messengerInputElement = document.activeElement;
+
+    if (messageInputElement !== undefined) {
+      messageInputElement = document.activeElement;
+    }
+
+    if (emojiSelectionWrapper !== undefined && emojiSelectionWrapper !== null) {
+      emojiSelectionWrapper.className = 'active';
+      emojiSelectionWrapper.tabIndex = -1;
+      emojiSelectionWrapper.focus();
+    } else {
+      const wrapper = createEmojiSelectionWrapper();
+      document.body.appendChild(wrapper);
+      wrapper.tabIndex = -1;
+      wrapper.focus();
+    }
+    
+    const firstSuggestion = document.getElementsByClassName('emoji-suggestion')[0];
+    // firstSuggestion.select();
+    firstSuggestion.className = 'emoji-suggestion-selected';
+    // If the wrapper is open, do some sort of tab index and arrow keys should navigate
+    // up and down the list.
+  }
+
   function createEmojiSuggestions(emojiSelectionWrapper) {
     let counter = 5;
 
@@ -49,6 +170,8 @@
               emojiAliasWrapper = document.createElement('div'),
               emojiValue = document.createElement('p'),
               emojiAlias = document.createElement('p');
+
+        state.suggestedEmojiList.push(emojiList[alias]);
 
         emojiSuggestion.setAttribute('class', 'emoji-suggestion');
 
@@ -80,22 +203,15 @@
     const messageBounds = messageBoundDiv.getBoundingClientRect();
     const bottom = messageBounds.height + 10;
 
-    // alert('bottom: ' + messageBounds.bottom);
-    // alert('y: ' + messageBounds.y);
-    // alert('height: ' + messageBounds.height);
-
     const emojiSelectionWrapper = document.createElement('div');
-    emojiSelectionWrapper.setAttribute('class', 'emoji-selection-wrapper');
+    emojiSelectionWrapper.setAttribute('id', 'emoji-selection-wrapper');
+    // emojiSelectionWrapper.setAttribute('class', 'active');
+    emojiSelectionWrapper.className = 'active';
     emojiSelectionWrapper.setAttribute('style', `bottom: ${bottom}px; left: ${messageBounds.left}px;`);
 
     createEmojiSuggestions(emojiSelectionWrapper);
 
     return emojiSelectionWrapper;
-  }
-
-  function showEmojiSuggestions() {
-    const wrapper = createEmojiSelectionWrapper();
-    document.body.appendChild(wrapper);
   }
 
   function searchInputForEmojiCode(inputText) {
@@ -130,14 +246,14 @@
     // If the previous message is the same as the new, clearly a conventional delete
     // didn't work. Resort to a forced delete by means of selecting all text and inserting
     // with a new string that matches what should've been deleted.
-    if ((prevMessageText.length === messageText.length) && prevMessageText.length > 0) {
+    if ((state.prevMessageText.length === messageText.length) && state.prevMessageText.length > 0) {
       handleDeleteText(messageText);
     }
   }
 
   function updatePrevStoredValues(event) {
     if (event.key === BACKSPACE_KEY) {
-      prevMessageText = document.activeElement.textContent;
+      state.prevMessageText = document.activeElement.textContent;
     }
 
     // This is part of a crazy hack for handling deleting when text has been inserted,
@@ -147,16 +263,16 @@
     // replaced where the first character typed after the replacement isn't shown on the
     // screen. The `lastPressedKey` is re-inserted after replacement so that it shows up.
     if (event.key.length === 1) {
-      lastKeyPressed = event.key;
+      state.lastKeyPressed = event.key;
     }
   }
 
   function checkForUnrenderedChars(event, messageText) {
     if (event.key !== BACKSPACE_KEY
         && messageText.length === 0
-        && shouldInsertLastTypedKey === true) {
-      document.execCommand('insertText', true, lastKeyPressed);
-      shouldInsertLastTypedKey = false;
+        && state.shouldInsertLastTypedKey === true) {
+      document.execCommand('insertText', true, state.lastKeyPressed);
+      state.shouldInsertLastTypedKey = false;
     }
   }
 
@@ -164,10 +280,12 @@
     const textSelection = document.getSelection().toString();
 
     // Case when one or multiple parts of the message are selected.
-    if (textSelection.length > 0
-        && prevMessageText.length > 1
-        && textSelection.length < prevMessageText.length) {
-      const splitMessage = prevMessageText.split(textSelection);
+    if (textSelection.length > 0 && 
+      state.prevMessageText.length > 1 &&
+      textSelection.length < state.prevMessageText.length) {
+      // Sara this breaks, you dumb bitch. It selects every occurence of whatever you've selected.
+    // https://stackoverflow.com/questions/18435088/get-selected-text-in-contenteditable-div
+      const splitMessage = state.prevMessageText.split(textSelection);
       let newMessage = '';
 
       for (let i = 0; i < splitMessage.length; i++) {
@@ -176,13 +294,14 @@
 
       document.execCommand('selectAll');
       document.execCommand('insertText', true, newMessage);
-    } else if (prevMessageText.length === 1 || textSelection.length === prevMessageText.length) {
+    } else if (state.prevMessageText.length === 1
+        || textSelection.length === state.prevMessageText.length) {
       // Case when entire string is selected or there's a single character
       document.execCommand('delete');
-      shouldInsertLastTypedKey = true;
+      state.shouldInsertLastTypedKey = true;
     } else {
       // Case when we're only deleting a single character
-      let newMessage = prevMessageText.substring(0, prevMessageText.length - 1);
+      let newMessage = state.prevMessageText.substring(0, state.prevMessageText.length - 1);
       document.execCommand('selectAll');
       document.execCommand('insertText', true, newMessage);
     }
